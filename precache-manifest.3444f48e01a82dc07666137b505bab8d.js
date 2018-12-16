103d6966b88febf78a3bfe5d9d21b38709aa2555
Eric Bender <etbender@edu.uwaterloo.ca>
2018-12-16 14:22:46 -0500

    Updates

---
+++ precache-manifest.3444f48e01a82dc07666137b505bab8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9751730c2e7546e7bf0589d6826e6d49",
+    "url": "/apple-clicker/static/media/sky-background.9751730c.jpg"
+  },
+  {
+    "revision": "1fc1361af720afc930b50ef5c85e3981",
+    "url": "/apple-clicker/static/media/apple.1fc1361a.png"
+  },
+  {
+    "revision": "73c94010fa0cd2935083",
+    "url": "/apple-clicker/static/js/runtime~main.73c94010.js"
+  },
+  {
+    "revision": "266f094fea8524284a76",
+    "url": "/apple-clicker/static/js/main.266f094f.chunk.js"
+  },
+  {
+    "revision": "c7e0ca36d3fa89e1dc3c",
+    "url": "/apple-clicker/static/js/1.c7e0ca36.chunk.js"
+  },
+  {
+    "revision": "266f094fea8524284a76",
+    "url": "/apple-clicker/static/css/main.cb371da9.chunk.css"
+  },
+  {
+    "revision": "0f32744f6cf77379673e515d570c9c27",
+    "url": "/apple-clicker/index.html"
+  }
+];
