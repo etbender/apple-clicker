92738534d211700a23845617dc0c8fd6d22c03f2
Eric Bender <etbender@edu.uwaterloo.ca>
2018-12-16 14:29:03 -0500

    Updates

---
+++ precache-manifest.6eaf569af2423dc56ccb2689581fe028.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9751730c2e7546e7bf0589d6826e6d49",
+    "url": "/game/static/media/sky-background.9751730c.jpg"
+  },
+  {
+    "revision": "1fc1361af720afc930b50ef5c85e3981",
+    "url": "/game/static/media/apple.1fc1361a.png"
+  },
+  {
+    "revision": "dffb2a4fcbf3ea7182df",
+    "url": "/game/static/js/runtime~main.dffb2a4f.js"
+  },
+  {
+    "revision": "d30d3c5e16ede655f331",
+    "url": "/game/static/js/main.d30d3c5e.chunk.js"
+  },
+  {
+    "revision": "c7e0ca36d3fa89e1dc3c",
+    "url": "/game/static/js/1.c7e0ca36.chunk.js"
+  },
+  {
+    "revision": "d30d3c5e16ede655f331",
+    "url": "/game/static/css/main.cb371da9.chunk.css"
+  },
+  {
+    "revision": "2be92f85b16ed80e2b7bd1f7f4277fdf",
+    "url": "/game/index.html"
+  }
+];
