859d897bf7dd4977820c10815bfbadc443c525de
Eric Bender <etbender@edu.uwaterloo.ca>
2018-12-17 17:21:28 -0500

    Updates

--- precache-manifest.eebb6aceaa0dda98484220010080a68e.js
+++ precache-manifest.eebb6aceaa0dda98484220010080a68e.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/apple-clicker/static/media/timer.d25a4846.png"
   },
   {
-    "revision": "9751730c2e7546e7bf0589d6826e6d49",
-    "url": "/apple-clicker/static/media/sky-background.9751730c.jpg"
+    "revision": "1e24d359c7474aa396af2616b82b351b",
+    "url": "/apple-clicker/static/media/sky-background.1e24d359.jpg"
   },
   {
     "revision": "1fc1361af720afc930b50ef5c85e3981",
@@ -16,19 +16,19 @@ self.__precacheManifest = [
     "url": "/apple-clicker/static/js/runtime~main.73c94010.js"
   },
   {
-    "revision": "89e38200b24294f22588",
-    "url": "/apple-clicker/static/js/main.89e38200.chunk.js"
+    "revision": "544120ef00228ae673e9",
+    "url": "/apple-clicker/static/js/main.544120ef.chunk.js"
   },
   {
     "revision": "9c891a6b83b741c0e97e",
     "url": "/apple-clicker/static/js/1.9c891a6b.chunk.js"
   },
   {
-    "revision": "89e38200b24294f22588",
+    "revision": "544120ef00228ae673e9",
     "url": "/apple-clicker/static/css/main.10aa03c6.chunk.css"
   },
   {
-    "revision": "bb6287a9aca954261b861ee930d155eb",
+    "revision": "789e8ebf383f9491c0c7cc335c3582a2",
     "url": "/apple-clicker/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/apple-clicker/precache-manifest.473c7112a801ac82fb78b0f2f2b5f591.js"
+  "/apple-clicker/precache-manifest.eebb6aceaa0dda98484220010080a68e.js"
 );
 
 workbox.clientsClaim();
